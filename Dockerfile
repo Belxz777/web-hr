412a33b9d7f244fca32a06ed580da5502510bbd3
Belxz777 <belxz888@gmail.com>
2025-07-21 00:11:15 +0400

    okay

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,35 @@
-FROM node:18-alpine AS builder
-
+FROM node:18-alpine AS deps
+RUN apk add --no-cache libc6-compat
 WORKDIR /app
 
-# 1. Копируем системные файлы
-COPY package*.json ./
-COPY tsconfig.json ./
-COPY next.config.mjs ./
+COPY package.json package-lock.json ./
+RUN  npm install --production
 
-# 2. Устанавливаем зависимости
-RUN npm ci
+FROM node:18-alpine AS builder
+WORKDIR /app
+COPY --from=deps /app/node_modules ./node_modules
+COPY . .
 
-# 3. Копируем исходный код
-COPY src ./src
-COPY public ./public
-COPY app ./app
-COPY .env ./
+ENV NEXT_TELEMETRY_DISABLED 1
 
-# 4. Собираем приложение
 RUN npm run build
 
-# Production образ
-FROM node:18-alpine
+FROM node:18-alpine AS runner
 WORKDIR /app
 
-COPY --from=builder /app/.next ./.next
+ENV NODE_ENV production
+ENV NEXT_TELEMETRY_DISABLED 1
+
+
+
+COPY --from=builder --chown=nextjs:nodejs /app/.next ./.next
 COPY --from=builder /app/node_modules ./node_modules
 COPY --from=builder /app/package.json ./package.json
-COPY --from=builder /app/public ./public
-COPY --from=builder /app/.env ./
 
-ENV NODE_ENV=production
+USER nextjs
+
 EXPOSE 3000
+
+ENV PORT 3000
+
 CMD ["npm", "start"]
--- next.config.mjs
+++ next.config.mjs
@@ -15,20 +15,20 @@ hostname: "images.squarespace-cdn.com",
 
 }]
   },
-    webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@lib': path.resolve(__dirname, 'src/lib'),
-      '@store': path.resolve(__dirname, 'src/store'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@svgs': path.resolve(__dirname, 'src/svgs'),
-      '@config': path.resolve(__dirname, 'src/config.ts'),
-    };
-    return config;
-  },
+  //   webpack: (config) => {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '@': path.resolve(__dirname, 'src'),
+  //     '@components': path.resolve(__dirname, 'src/components'),
+  //     '@utils': path.resolve(__dirname, 'src/utils'),
+  //     '@lib': path.resolve(__dirname, 'src/lib'),
+  //     '@store': path.resolve(__dirname, 'src/store'),
+  //     '@hooks': path.resolve(__dirname, 'src/hooks'),
+  //     '@svgs': path.resolve(__dirname, 'src/svgs'),
+  //     '@config': path.resolve(__dirname, 'src/config.ts'),
+  //   };
+  //   return config;
+  // },
 
   async rewrites() {
     return [
